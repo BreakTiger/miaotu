0c310b79fe0c78b897050d9a7e67c4a069995099
BreakTiger <46531043+BreakTiger@users.noreply.github.com>
2019-12-17 16:06:24 +0800

    tiger 12-17

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,62 +9,83 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    sw_list: [1, 2], //轮播图
+    na_list: [{
+        icon: '',
+        name: '单身拼团',
+        path: '',
+      },
+      {
+        icon: '',
+        name: '助力免单',
+        path: '',
+      },
+      {
+        icon: '',
+        name: '门票砍价',
+        path: '',
+      },
+      {
+        icon: '',
+        name: '现金签到',
+        path: '',
+      }
+    ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    
+  onLoad: function(options) {
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-    
+  onReady: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-    
+  onShow: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-    
+  onHide: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-    
+  onUnload: function() {
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-    
+  onPullDownRefresh: function() {
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-    
+  onReachBottom: function() {
+
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-    
+  onShareAppMessage: function() {
+
   }
 })
