1e8741f9c03d1ca8fdcaf7982dd0d904f256ed9e
BreakTiger <46531043+BreakTiger@users.noreply.github.com>
2019-12-18 14:33:12 +0800

    tiger 12-18

--- pages/index/place/place.js
+++ pages/index/place/place.js
@@ -5,62 +5,192 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    leftlist: [{
+        id: 0,
+        name: '热门'
+      },
+      {
+        id: 1,
+        name: '广州'
+      },
+      {
+        id: 2,
+        name: '北京'
+      },
+      {
+        id: 3,
+        name: '上海'
+      }
+    ],
+    choice_one: 0,
+    rightlist: [{
+        id: 0,
+        imgpath: '../../../resources/1.png',
+        name: '北京',
+        spell: 'beijing'
+      },
+      {
+        id: 0,
+        imgpath: '../../../resources/1.png',
+        name: '北京',
+        spell: 'beijing'
+      }, {
+        id: 0,
+        imgpath: '../../../resources/1.png',
+        name: '北京',
+        spell: 'beijing'
+      }, {
+        id: 0,
+        imgpath: '../../../resources/1.png',
+        name: '北京',
+        spell: 'beijing'
+      }, {
+        id: 0,
+        imgpath: '../../../resources/1.png',
+        name: '北京',
+        spell: 'beijing'
+      }, {
+        id: 0,
+        imgpath: '../../../resources/1.png',
+        name: '北京',
+        spell: 'beijing'
+      }, {
+        id: 0,
+        imgpath: '../../../resources/1.png',
+        name: '北京',
+        spell: 'beijing'
+      }, {
+        id: 0,
+        imgpath: '../../../resources/1.png',
+        name: '北京',
+        spell: 'beijing'
+      }, {
+        id: 0,
+        imgpath: '../../../resources/1.png',
+        name: '北京',
+        spell: 'beijing'
+      }, {
+        id: 0,
+        imgpath: '../../../resources/1.png',
+        name: '北京',
+        spell: 'beijing'
+      }, {
+        id: 0,
+        imgpath: '../../../resources/1.png',
+        name: '北京',
+        spell: 'beijing'
+      }, {
+        id: 0,
+        imgpath: '../../../resources/1.png',
+        name: '北京',
+        spell: 'beijing'
+      }, {
+        id: 0,
+        imgpath: '../../../resources/1.png',
+        name: '北京',
+        spell: 'beijing'
+      }, {
+        id: 0,
+        imgpath: '../../../resources/1.png',
+        name: '北京',
+        spell: 'beijing'
+      }, {
+        id: 0,
+        imgpath: '../../../resources/1.png',
+        name: '北京',
+        spell: 'beijing'
+      }, {
+        id: 0,
+        imgpath: '../../../resources/1.png',
+        name: '北京',
+        spell: 'beijing'
+      }, {
+        id: 0,
+        imgpath: '../../../resources/1.png',
+        name: '北京',
+        spell: 'beijing'
+      }, {
+        id: 0,
+        imgpath: '../../../resources/1.png',
+        name: '北京',
+        spell: 'beijing'
+      }, {
+        id: 0,
+        imgpath: '../../../resources/1.png',
+        name: '北京',
+        spell: 'beijing'
+      }, {
+        id: 0,
+        imgpath: '../../../resources/1.png',
+        name: '北京',
+        spell: 'beijing'
+      }, {
+        id: 0,
+        imgpath: '../../../resources/1.png',
+        name: '北京',
+        spell: 'beijing'
+      }, {
+        id: 0,
+        imgpath: '../../../resources/1.png',
+        name: '北京',
+        spell: 'beijing'
+      }
+    ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
--- pages/index/search/search.js
+++ pages/index/search/search.js
@@ -1,66 +1,110 @@
-// pages/index/search/search.js
+const request = require('../../../api/http.js')
+import modals from '../../../methods/modal.js'
+const app = getApp()
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    hislist: [{
+        id: 1,
+        name: '西藏骑行'
+      },
+      {
+        id: 2,
+        name: '北京阅兵'
+      },
+      {
+        id: 3,
+        name: '长隆一日游'
+      },
+      {
+        id: 4,
+        name: '广州塔'
+      },
+      {
+        id: 5,
+        name: '普吉岛'
+      }
+    ],
+    hotlist: [{
+        id: 1,
+        name: '西藏骑行'
+      },
+      {
+        id: 2,
+        name: '北京阅兵'
+      },
+      {
+        id: 3,
+        name: '长隆一日游'
+      },
+      {
+        id: 4,
+        name: '广州塔'
+      },
+      {
+        id: 5,
+        name: '普吉岛'
+      }
+    ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
