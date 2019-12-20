2287822f8a9f3f9266e4dd0d38ca75d67efc18e0
BreakTiger <46531043+BreakTiger@users.noreply.github.com>
2019-12-20 15:46:41 +0800

    tiger 12-20

--- pages/index/goods/goods_buy/goods_buy.js
+++ pages/index/goods/goods_buy/goods_buy.js
@@ -1,4 +1,4 @@
-// pages/index/goods/goods.js
+// pages/index/goods/goods_buy/goods_buy.js
 Page({
 
   /**
@@ -11,74 +11,56 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function(options) {
+  onLoad: function (options) {
 
-  },
-
-  toEvaluate: function() {
-    wx.navigateTo({
-      url: '/pages/index/goods/evaluate/evaluate',
-    })
-  },
-
-  toShop:function(){
-    wx.navigateTo({
-      url: '/pages/index/goods/shop/shop',
-    })
-  },
-
-  toOrder:function(){
-    wx.navigateTo({
-      url: '/pages/index/goods/goods_buy/goods_buy',
-    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function() {
+  onReady: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function() {
+  onShow: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function() {
+  onHide: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function() {
+  onUnload: function () {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function() {
+  onPullDownRefresh: function () {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function() {
+  onReachBottom: function () {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function() {
+  onShareAppMessage: function () {
 
   }
 })
