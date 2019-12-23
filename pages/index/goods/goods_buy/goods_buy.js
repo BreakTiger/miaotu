5a47d615e1075a25c33a17ed5ca4a306d895142f
BreakTiger <46531043+BreakTiger@users.noreply.github.com>
2019-12-23 15:49:47 +0800

    tiger 12-23

--- pages/index/goods/goods_buy/goods_buy.js
+++ pages/index/goods/goods_buy/goods_buy.js
@@ -1,7 +1,56 @@
-var date = new Date();
-var year = date.getFullYear()
-var month = date.getMonth()
-var day = date.getDate()
+var curDate = new Date();
+var curYear = curDate.getFullYear();
+var curMonth = curDate.getMonth();
+var curDay = curDate.getDate()
+var curWeek = curDate.getDay();
+
+//月份天数表
+var DAY_OF_MONTH = [
+  [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
+  [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+];
+
+
+//判断当前年是否闰年
+var isLeapYear = function(year) {
+  if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
+    return 1
+  else
+    return 0
+};
+
+//获取当月有多少天
+var getDayCount = function(year, month) {
+  return DAY_OF_MONTH[isLeapYear(year)][month];
+};
+
+//获取当前索引下是几号
+var getDay = function(index) {
+  return index - curDayOffset;
+};
+
+var pageData = {
+  date: "", //当前日期字符串
+  arrIsShow: [], //是否显示此日期
+  arrDays: [], //关于几号的信息
+  arrPrice: [] //对应日期的价格数组
+}
+
+
+//刷新全部数据
+var refreshPageData = function(year, month, day, week) {
+  pageData.date = year + '年' + (month + 1) + '月' + day + '日'
+  var offset = new Date(year, month, 1).getDay();
+  for (var i = 0; i < 42; ++i) {
+    pageData.arrIsShow[i] = i < offset || i >= getDayCount(year, month) + offset ? false : true;
+    pageData.arrDays[i] = i - offset + 1;
+  }
+};
+
+
+
+refreshPageData(curYear, curMonth, curDay, curWeek);
+
 
 Page({
 
@@ -45,62 +94,58 @@ Page({
       }
     ],
     choice_two: 1,
-    todayTime: '',
-    week: ['M', 'T', 'W', 'T', 'F', 'S', 'S']
+    pageData,
+    adult: 0, //成人票数
+    child: 0, //儿童票数 
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-    this.getToday()
+
   },
 
-  getToday: function() {
-    var time = year + '年' + (month + 1) + '月' + day + '日'
+  // 上一个月
+  lastMonth: function() {
+    if (0 == curMonth) {
+      curMonth = 11;
+      --curYear
+    } else {
+      --curMonth;
+    }
+    refreshPageData(curYear, curMonth, 1, 0);
     this.setData({
-      todayTime: time
+      pageData: pageData
+    })
+  },
+  // 下一个月
+  nextMonth: function() {
+    if (11 == curMonth) {
+      curMonth = 0;
+      ++curYear
+    } else {
+      ++curMonth;
+    }
+    refreshPageData(curYear, curMonth, 1, 0);
+    this.setData({
+      pageData: pageData
     })
   },
 
-  lastMonth: function() {
-    if (0 == month) {
-      month = 11;
-      --year
-      var time = year + '年' + (month + 1) + '月' + 1 + '日'
-      this.setData({
-        todayTime: time
-      })
-    } else {
-      --month;
-      var time = year + '年' + (month + 1) + '月' + 1 + '日'
-      this.setData({
-        todayTime: time
-      })
-    }
+  toadd: function() {
+
   },
 
-  nextMonth: function() {
-    if (11 == month) {
-      month = 0;
-      ++year
-      var time = year + '年' + (month + 1) + '月' + 1 + '日'
-      this.setData({
-        todayTime: time
-      })
-    } else {
-      ++month;
-      var time = year + '年' + (month + 1) + '月' + 1 + '日'
-      this.setData({
-        todayTime: time
-      })
-    }
+  tominus: function() {
+
   },
 
-  // 判断当前是否为闰年
-
-
-  // 获取当前月份的天数
+  toOrder: function() {
+    wx.navigateTo({
+      url: '/pages/index/goods/order/order',
+    })
+  },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
