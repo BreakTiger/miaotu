5fbb0dcd184ec2142af6ca3e931c7ca25f2ce7fc
BreakTiger <46531043+BreakTiger@users.noreply.github.com>
2019-12-25 11:35:35 +0800

    tiger 12-25

--- pages/mine/order/order.js
+++ pages/mine/order/order.js
@@ -5,62 +5,78 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    navlist: [{
+      id: 1,
+      name: '全部',
+    }, {
+      id: 2,
+      name: '待付款',
+    }, {
+      id: 3,
+      name: '待分享',
+    }, {
+      id: 4,
+      name: '待出行',
+    }, {
+      id: 5,
+      name: '待评价',
+    }],
+    choice_one:1
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
