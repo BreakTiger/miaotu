552b17998c4806f267fcbfc3efa3fb45f882ae4a
BreakTiger <46531043+BreakTiger@users.noreply.github.com>
2019-12-27 18:31:34 +0800

    tiger 12-27

--- pages/mine/members/members.js
+++ pages/mine/members/members.js
@@ -5,62 +5,104 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    cardlist: [{
+        id: 1,
+        img: '../../../resources/card_one.png',
+        money: 99,
+        card: '大众会员',
+        cardType: '8.5折会员卡',
+        state: '首单最高立减666元'
+      },
+      {
+        id: 2,
+        img: '../../../resources/card_two.png',
+        money: 99,
+        card: '黄金会员',
+        cardType: '8.5折会员卡',
+        state: '首单最高立减666元'
+      },
+      {
+        id: 3,
+        img: '../../../resources/card_three.png',
+        money: 99,
+        card: '铂金会员',
+        cardType: '8.5折会员卡',
+        state: '首单最高立减666元'
+      },
+      {
+        id: 4,
+        img: '../../../resources/card_four.png',
+        money: 99,
+        card: '钻石会员',
+        cardType: '8.5折会员卡',
+        state: '首单最高立减666元'
+      },
+      {
+        id: 5,
+        img: '../../../resources/card_five.png',
+        money: 99,
+        card: '至尊会员',
+        cardType: '8.5折会员卡',
+        state: '首单最高立减666元'
+      }
+    ],
+    top:160,
+    move:70
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
