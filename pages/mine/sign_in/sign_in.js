5e9f1ba09d4301195df7c70f5324a80db763cde4
BreakTiger <46531043+BreakTiger@users.noreply.github.com>
2019-12-28 14:02:05 +0800

    tiger 12-28

--- pages/mine/sign_in/sign_in.js
+++ pages/mine/sign_in/sign_in.js
@@ -1,4 +1,57 @@
-// pages/mine/sign_in/sign_in.js
+var curDate = new Date();
+var curYear = curDate.getFullYear();
+var curMonth = curDate.getMonth();
+var curDay = curDate.getDate()
+var curWeek = curDate.getDay();
+
+//月份天数表
+var DAY_OF_MONTH = [
+  [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
+  [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+];
+
+//判断当前年是否闰年
+var isLeapYear = function (year) {
+  if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
+    return 1
+  else
+    return 0
+};
+
+//获取当月有多少天
+var getDayCount = function (year, month) {
+  return DAY_OF_MONTH[isLeapYear(year)][month];
+};
+
+//获取当前索引下是几号
+var getDay = function (index) {
+  return index - curDayOffset;
+};
+
+var pageData = {
+  date: "", //当前日期字符串
+  arrIsShow: [], //是否显示此日期
+  arrDays: [], //关于几号的信息
+  arrSign: [] //对应日期的签到状态数组
+}
+
+
+//刷新全部数据
+var refreshPageData = function (year, month, day, week) {
+  pageData.date = year + '年' + (month + 1) + '月' + day + '日'
+  var offset = new Date(year, month, 1).getDay();
+  for (var i = 0; i < 42; ++i) {
+    pageData.arrIsShow[i] = i < offset || i >= getDayCount(year, month) + offset ? false : true;
+    pageData.arrDays[i] = i - offset + 1;
+  }
+};
+
+
+
+refreshPageData(curYear, curMonth, curDay, curWeek);
+
+
+
 Page({
 
   /**
