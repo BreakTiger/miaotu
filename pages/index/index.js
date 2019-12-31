1079b5e009bde9f2313ee91e96841dff581c661e
BreakTiger <46531043+BreakTiger@users.noreply.github.com>
2019-12-31 15:45:26 +0800

    tiger 12-31

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,12 +2,12 @@ const request = require('../../api/http.js')
 import modals from '../../methods/modal.js'
 const app = getApp()
 
-// 地图实例化
-// var QQMapWX = require('../../qqmap-wx-jssdk.js');
-
-// var qqmapsdk = new QQMapWX({
-//   key: 'OSCBZ-J26WX-M6M4B-T6MQM-JC6EQ-HUFDP'
-// });
+// 引入SDK核心类
+var QQMapWX = require('../../qqmap-wx-jssdk.min.js')
+// 实例化API核心类
+var demo = new QQMapWX({
+  key: 'OSCBZ-J26WX-M6M4B-T6MQM-JC6EQ-HUFDP'
+});
 
 
 Page({
@@ -40,25 +40,32 @@ Page({
       }
     ],
     skillgoods: {},
+    countdown: '',
     goodsnav: [{
-      id: 5,
-      name: '推荐'
-    }, {
-      id: 6,
-      name: '最新'
-    }, {
-      id: 1,
-      name: '本地'
-    }, {
-      id: 2,
-      name: '国外'
-    }, {
-      id: 3,
-      name: '小众'
-    }, {
-      id: 4,
-      name: '猎奇'
-    }],
+        id: 5,
+        name: '推荐'
+      },
+      {
+        id: 6,
+        name: '最新'
+      },
+      {
+        id: 1,
+        name: '本地'
+      },
+      {
+        id: 2,
+        name: '国外'
+      },
+      {
+        id: 3,
+        name: '小众'
+      },
+      {
+        id: 4,
+        name: '猎奇'
+      }
+    ],
     choice_one: 5,
     page: 1,
     goods: []
@@ -69,21 +76,36 @@ Page({
    */
   onLoad: function(options) {
     this.positioning()
-
-
+    this.getBanner()
   },
 
   // 获取定位
   positioning: function() {
+    let that = this
     wx.getLocation({
+      type: 'gcj02',
       success: function(res) {
         let lat = res.latitude
         let lon = res.longitude
-        // console.log('lat:', lat)
-        // console.log('lon:', lon)
+        console.log(lat, lon)
+        // 使用地图API
+        demo.reverseGeocoder({
+          location: {
+            latitude: res.latitude,
+            longitude: res.longitude
+          },
+          success: function(res) {
+            console.log(res);
+          },
+          fail: function(error) {
+            console.log(error);
+          },
+          complete: function(res) {
+            console.log(res);
+          }
+        })
       },
     })
-    this.getBanner()
   },
 
   // 获取轮播
@@ -120,8 +142,11 @@ Page({
         that.setData({
           skillgoods: res.data.data
         })
+        let start = (new Date(res.data.data.ms_starttime)).getTime()
+        let end = (new Date(res.data.data.ms_endtime)).getTime()
+        that.countdown(start, end)
         //默认情况下：
-        that.getList(that.data.choice_one)
+        // that.getList(that.data.choice_one)
       } else {
         wx.showToast({
           title: '系统繁忙，请稍后重试',
@@ -129,6 +154,31 @@ Page({
         })
       }
     })
+  },
+
+  // 倒计时
+  countdown: function(start, end) {
+    let that = this
+    let now = Date.parse(new Date())
+    let ts = parseInt((end - now) / 1000)
+    // 判断活动是否开始 当前时间处在活动开始时间，活动结束时间这俩个时间区间内
+    if (now >= start || now <= end) {
+      let days = Math.floor(ts / (60 * 60 * 24));
+      let modulo = ts % (60 * 60 * 24);
+      let hours = Math.floor(modulo / (60 * 60));
+      modulo = modulo % (60 * 60);
+      let minutes = Math.floor(modulo / 60);
+      let seconds = modulo % 60;
+      let times = days + ':' + hours + ':' + minutes + ':' + seconds
+      that.setData({
+        countdown: times
+      })
+    } else if (now > end) { //当前时间超出活动时间，重新请求爆款秒杀接口，更新商品
+      that.getSkill()
+    }
+    setTimeout(function() {
+      that.countdown(start, end)
+    }, 1000)
   },
 
   // 获取分类列表
