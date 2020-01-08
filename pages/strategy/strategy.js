8ebbf29202553d640f81c30c524669c88ba2f5d4
BreakTiger <46531043+BreakTiger@users.noreply.github.com>
2020-01-08 18:43:52 +0800

    tiger 1-8

--- pages/strategy/strategy.js
+++ pages/strategy/strategy.js
@@ -1,66 +1,75 @@
-// pages/strategy/strategy.js
+const request = require('../../api/http.js')
+import modals from '../../methods/modal.js'
+const app = getApp()
+
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    tlist: [{
+        id: 1,
+        name: '关注'
+      },
+      {
+        id: 2,
+        name: '发现'
+      },
+      {
+        id: 3,
+        name: '问答'
+      },
+      {
+        id: 4,
+        name: '精选'
+      }
+    ],
+    choice_one: 1
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
 
+  onLoad: function(options) {
+    // 判断用户是否登陆
+    let openID = wx.getStorageSync('openid') || ''
+    if (!openID) {
+      wx.navigateTo({
+        url: '/pages/login/login',
+      })
+    } else {
+
+    }
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
 
+  // 发布
+  tosend: function() {
+    wx.navigateTo({
+      url: '/pages/strategy/send/send',
+    })
   },
+
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
 
-  },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
 
-  }
 })
