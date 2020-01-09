19af58d5bffe660e9733537a3f936670d86626ae
BreakTiger <46531043+BreakTiger@users.noreply.github.com>
2020-01-09 15:29:38 +0800

    tiger 1-9

---
+++ pages/send/send.js
@@ -0,0 +1,25 @@
+const request = require('../../api/http.js')
+import modals from '../../methods/modal.js'
+const app = getApp()
+
+Page({
+
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+})
--- pages/strategy/strategy.js
+++ pages/strategy/strategy.js
@@ -187,23 +187,24 @@ Page({
   // 发布
   tosend: function() {
     wx.navigateTo({
-      url: '/pages/strategy/send/send',
+      url: '/pages/send/send',
     })
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
   onPullDownRefresh: function() {
-
+    wx.showToast({
+      title: '加载中',
+      icon: 'loading',
+      duration: 1000
+    })
+    setTimeout(() => {
+      wx.stopPullDownRefresh()
+    }, 1000);
+    this.onLoad();
   },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
+
   onReachBottom: function() {
 
-  },
-
-
+  }
 })
